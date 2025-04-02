db2c918a10082e29da7715859093b9faa942a334
Isaqueantoniodasilva <isaque.silva23@etepd.com>
2025-04-02 13:43:03 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,32 +1,32 @@
-print("Você está em uma floresta escura. Há dois caminhos à frente.")
+print("Você está em uma basilica escura. Há dois caminhos à frente.")
 print("1. Seguir pelo caminho da direita.")
 print("2. Seguir pelo caminho da esquerda.")
 
 escolha = input("Digite 1 ou 2: ")
 
 if escolha == "1":
-    print("\nVocê encontra um rio. O que deseja fazer?")
-    print("1. Tentar atravessar a nado.")
-    print("2. Procurar uma ponte.")
+    print("\nVocê encontra um objeto mistico. O que deseja fazer?")
+    print("1. pegar o abjeto.")
+    print("2. seguir em frente.")
 
     escolha = input("Digite 1 ou 2: ")
 
     if escolha == "1":
-        print("\nA correnteza está forte e você se afoga. Fim de jogo!")
+        print("\num ser mistico sai do abjeto")
     else:
-        print("\nVocê encontra uma ponte e atravessa em segurança. Parabéns, você sobreviveu!")
+        print("\nnada acontecera")
 
 elif escolha == "2":
-    print("\nVocê entra em uma caverna escura. Você vê algo brilhando.")
-    print("1. Se aproximar para investigar.")
-    print("2. Ignorar e continuar andando.")
+    print("\nvoce recebera um desejo ")
+    print("1. qual pedido voce fara")
+    print("2. Ignorar o ser mistico e continuar andando.")
 
     escolha = input("Digite 1 ou 2: ")
 
     if escolha == "1":
-        print("\nEra um tesouro! Você fica rico. Parabéns!")
+        print("\nseu desejo e se tornar rico")
     else:
-        print("\nUm urso aparece e te ataca. Fim de jogo!")
+        print("\nvoce segue a sua vida monotona")
 
 else:
     print("\nOpção inválida. O jogo terminou.")
